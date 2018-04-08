51fff7160b6c07c2e3a0aefec8512c702ddacbda
David Juarez <davidjuarez1411@gmail.com>
2018-04-08 16:32:06 -0700

    added some CSS animation and a JavaScript function that starts the animation on scroll

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+	/*--------------------
+	Animate on Scroll
+	--------------------*/
+	$(window).scroll(function() {
+			$('.divTitle h3').each(function(){
+			var imagePos = $(this).offset().top;
+
+			var topOfWindow = $(window).scrollTop();
+				if (imagePos < topOfWindow+400) {
+					$(this).addClass("fadeIn");
+				}
+			});
+		});
+});
